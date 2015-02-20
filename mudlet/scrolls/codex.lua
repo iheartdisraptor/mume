06b06221cd71b6f5010ba270f9323250dcbd8226
Brynniel <iheartdisraptor@gmail.com>
2015-02-20 10:19:49 -0800

    Added codex (new manifest).

---
+++ mudlet/scrolls/codex.lua
@@ -0,0 +1,9 @@
+return {{[ [[scrolls]] ] = {2},},
+{[ [[clock]] ] = {3},[ [[scrolls]] ] = {4},[ [[time]] ] = {5},},
+{[ [[authors]] ] = {6},[ [[subtitle]] ] = [[Advanced MUME Time]],[ [[name]] ] = [[Clock]],[ [[url]] ] = [[https://github.com/iheartdisraptor/mume/raw/master/mudlet/scrolls/Clock.zip]],[ [[help]] ] = [[This script tracks the MUME time as well as the MUME date, allowing it to report, in addition to the current time, the dawn and dusk times, the real time to dawn and dusk, the date, and the season. In addition, the clock will optionally let you know every time a minute has passed and keep you informed as to the date and season every 24 minutes.]],[ [[version]] ] = [[1.0]],},
+{[ [[authors]] ] = {7},[ [[subtitle]] ] = [[Mudlet Package Manager]],[ [[name]] ] = [[Scrolls]],[ [[url]] ] = [[https://github.com/iheartdisraptor/mume/raw/master/mudlet/scrolls/Scrolls/1.0/Scrolls.zip]],[ [[help]] ] = [[Create, install, update, save, load, and reload Mudlet packages (called scrolls).]],[ [[version]] ] = [[1.0]],},
+{[ [[authors]] ] = {8},[ [[subtitle]] ] = [[Basic Time Functions]],[ [[name]] ] = [[Time]],[ [[url]] ] = [[https://github.com/iheartdisraptor/mume/raw/master/mudlet/scrolls/Time.zip]],[ [[help]] ] = [[This scroll contains basic functions for working with time.]],[ [[version]] ] = [[1.0]],},
+{[1] = [[Octavia]],},
+{[1] = [[Octavia]],},
+{[1] = [[Octavia]],},
+}
